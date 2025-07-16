93cc86e3711e1df294f6b6996a31ebd8a6abe19a
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-07-16 18:22:31 +0800

    Edited migration

---
+++ database/migrations/2025_07_16_084741_create_tbl_stamps_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_stamps', function (Blueprint $table) {
+            $table->id('stp_id');
+            $table->string('stp_to', 100);
+            $table->string('stp_from', 100);
+            $table->text('stp_message');
+            $table->string('stp_color', 20)->default('blue');
+            $table->string('stp_edit_code', 10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_stamps');
+    }
+};
