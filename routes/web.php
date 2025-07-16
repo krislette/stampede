f3a54196e3a4853998a4034f5973a9c92b9ad47b
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-07-16 18:22:13 +0800

    Initial commit for all routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StampController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [StampController::class, 'showWall'])->name('wall');
+Route::get('/create-stamp', [StampController::class, 'showCreateForm'])->name('create-stamp');
+Route::post('/create-stamp', [StampController::class, 'storeStamp'])->name('store-stamp');
+Route::get('/edit-stamp/{intStampId}', [StampController::class, 'showEditForm'])->name('edit-stamp');
+Route::post('/edit-stamp/{intStampId}', [StampController::class, 'updateStamp'])->name('update-stamp');
+Route::post('/delete-stamp/{intStampId}', [StampController::class, 'deleteStamp'])->name('delete-stamp');
+Route::get('/load-more-stamps', [StampController::class, 'loadMoreStamps'])->name('load-more-stamps');
