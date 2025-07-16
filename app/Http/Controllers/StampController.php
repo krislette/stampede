8f377aedace5ad9be821f1a210f811166322e18f
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-07-16 15:56:04 +0800

    Initial commit

---
+++ app/Http/Controllers/StampController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stamp;
+use Illuminate\Http\Request;
+
+class StampController
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Stamp $stamp)
+    {
+    }
+
+    public function edit(Stamp $stamp)
+    {
+    }
+
+    public function update(Request $request, Stamp $stamp)
+    {
+    }
+
+    public function destroy(Stamp $stamp)
+    {
+    }
+}
